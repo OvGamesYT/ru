784959fab52bb3905053c91fb1aceeee9f3190df
OvGamesYT <vova40227@gmail.com>
2021-02-23 13:05:10 +0600

    Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,26 @@
 /*
-	Forty by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+    Forty by HTML5 UP
+    html5up.net | @ajlkn
+    Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
-
 (function($) {
+    // scroller
+    var prevScrollpos = window.pageYOffset;
+    window.onscroll = function() {
+        var currentScrollPos = window.pageYOffset;
+        if (window.pageYOffset == 0) {
+            document.getElementById("header").style.top = "0";
+        } else {
+            if (prevScrollpos > currentScrollPos) {
+                document.getElementById("header").style.top = "0";
+            } else {
+                document.getElementById("header").style.top = "-75px";
+            }
+            prevScrollpos = currentScrollPos;
+        }
+    }
 
+    // var
     var $window = $(window),
         $body = $('body'),
         $wrapper = $('#wrapper'),
